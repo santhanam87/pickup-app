a8e8b444a37953c397b97112b8396fad124a50e2
Santhanam Elumalai <sansandy04@gmail.com>
2025-04-06 19:25:20 -0600

    Revert "[Feat] ATM Challenge"

--- src/app/_training/condition.ts
+++ src/app/_training/condition.ts
@@ -30,23 +30,18 @@ export function shop() {
  * Phase 2:
  * See or think about how you can do this for multiple users.
  */
-//
-const account = 100;
+const acount = 100;
 export function ATMChallange() {
   function ATM(action: string, amount: number) {
     if (action == "debit") {
-      return `you deposited $${amount}", your new balace is $${
-        account + amount
-      }.`;
+      return acount + amount;
     }
-    if (action == "credit" && account > amount) {
-      return `you credited $${amount},your new balence is $${
-        account - amount
-      }.`;
+    if (action == "credit" && acount > amount) {
+      return acount - amount;
     } else {
-      return `you don't have suffient money.You wish to take out $${amount} but you only have $${account}.`;
+      return "you don't have suffient money.";
     }
   }
-  console.info(ATM("credit", 111));
-  console.info("your transation is complete. Have a good day.");
+
+  console.info(ATM("debit", 111));
 }
