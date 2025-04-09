3af6f4d98969caea3a7e3b1a3ee5b0bf493204f7
Naveeth khan F <farnavith@gmail.com>
2025-04-09 23:48:10 +0530

    ATMChallenge
    I had corrected all the errors and coded as per the requirements of a ATM machine where single user or multiple users can credit and debit money based on their needs .

--- src/app/_training/condition.ts
+++ src/app/_training/condition.ts
@@ -1,23 +1,3 @@
-// goto shop if they have onion by 2 oninon
-// else if there is tomoto then by tomoto.
-// else return back.
-
-export function shop() {
-  const onion = 3;
-  const isOnionIsGood = true;
-  const tomoto = 3;
-  if (onion > 0 || tomoto > 0) {
-    console.info("enter a shope");
-    if (onion > 0 && isOnionIsGood) {
-      return "I got oninons";
-    } else if (tomoto > 0) {
-      return "I got tomoto";
-    }
-  } else {
-    return "Home";
-  }
-}
-
 /**
  * ATM machine
  * Phase 1:
@@ -28,25 +8,73 @@ export function shop() {
  * if it is credit
  * return your actual balance based on the available balance.
  * Phase 2:
- * See or think about how you can do this for multiple users.
+ * How you can I do this for multiple users.
  */
-//
-const account = 100;
-export function ATMChallange() {
-  function ATM(action: string, amount: number) {
-    if (action == "debit") {
-      return `you deposited $${amount}", your new balace is $${
-        account + amount
-      }.`;
+let  balance=10000;
+export function ATMChallange()
+ {
+  function ATM1(SelectType:"credit"|"debit",amount:number){
+  if (SelectType=="debit"){
+    if (balance>=amount){
+      balance-=amount;
+      console.info("Amount Withdrawn");
+      return `Remaining Amount:${balance}`;
     }
-    if (action == "credit" && account > amount) {
-      return `you credited $${amount},your new balence is $${
-        account - amount
-      }.`;
-    } else {
-      return `you don't have suffient money.You wish to take out $${amount} but you only have $${account}.`;
-    }
+    else{
+      return "You have insufficient balance";
+    }}
+  else if(SelectType=="credit") {
+    balance+=amount;
+    console.info("Amount Credited");
+    return `Remaining Amount:${balance}`;
   }
-  console.info(ATM("credit", 111));
-  console.info("your transation is complete. Have a good day.");
+  else {
+    return "Invalid Account type"
+  }
 }
+console.info(ATM1("debit",100));
+console.info(ATM1("credit",100));
+console.info(ATM1("debit",100));
+
+console.info("---------------------------\n");
+
+//ATMChallenge for multiple users 
+interface accounts{
+  userID:string;
+  balance:number;
+}
+const users: Record<string, accounts>={
+  user1: {userID:"Leo",balance:3000},
+  user2: {userID:"Jeevan",balance:7000},
+  user3: {userID:"Sanjay",balance:2000}
+};
+ 
+  function ATM2(userID:string,SelectType:"credit"|"debit",amount:number){
+    const user = users[userID];
+    if(!user){
+      console.info(`user is not found`);
+      return;
+  }
+  if (SelectType=="debit"){
+    if (user.balance>=amount){
+      user.balance-=amount;
+      console.info("Amount Withdrawn");
+      return `Remaining Amount:${user.balance}`;
+    }
+    else{
+      return "You have insufficient balance";
+    }}
+  else if(SelectType=="credit") {
+    user.balance+=amount;
+    console.info("Amount Credited");
+    return `Total Amount:${user.balance}`;
+  }
+    else {
+      return "Invalid Account type"
+  }
+}
+console.info(ATM2("user1","credit",1000));
+console.info(ATM2("Sanjay","credit",2000));
+console.info(ATM2("user3","debit",3000));
+console.info(ATM2("user2","debit",4000));
+ }
