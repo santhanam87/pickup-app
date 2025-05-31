23969a562616fcd722d564816b07f87393ec8a25
Santhanam Elumalai <sansandy04@gmail.com>
2025-05-30 21:05:23 -0600

    toy store

---
+++ src/app/api/toys/route.ts
@@ -0,0 +1,54 @@
+import { NextRequest } from "next/server";
+
+const toysInventory: string[] = ["gorgu", "choper", "darthMaul"];
+
+function getToys() {
+  return new Promise<string[]>((resolve) => {
+    resolve(toysInventory);
+  });
+}
+
+function addToy(toyName: string) {
+  return new Promise<string[]>((resolve) => {
+    toysInventory.push(toyName);
+    resolve(toysInventory);
+  });
+}
+
+function connectDB() {
+  return new Promise<true>((resolve) => {
+    resolve(true);
+    // rejects(false);
+  });
+}
+
+const toyDB: {
+  getToys: () => Promise<string[]>;
+  connectDB: () => Promise<boolean>;
+  addToy: (toyName: string) => Promise<string[]>;
+} = {
+  getToys,
+  connectDB,
+  addToy,
+};
+
+export async function PUT(request: NextRequest) {
+  try {
+    await toyDB.connectDB();
+    const requestBody = await request.json();
+    const toys = await toyDB.addToy(requestBody.toyName);
+    return Response.json({ toys });
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
+
+export async function GET() {
+  try {
+    await toyDB.connectDB();
+    const toys = await toyDB.getToys();
+    return Response.json({ toys });
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
