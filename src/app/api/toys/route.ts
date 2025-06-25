08ca8f3429f86b0853ed3b15d83e771197043e26
Naveeth khan F <farnavith@gmail.com>
2025-06-25 16:55:17 +0530

    Shop task
    I have completed the shop task to create ,edit, update and also delete the things using GET, PUT, POST, and DELETE

--- src/app/api/toys/route.ts
+++ src/app/api/toys/route.ts
@@ -1,17 +1,36 @@
 import { NextRequest } from "next/server";
 
-const toysInventory: string[] = ["gorgu", "choper", "darthMaul"];
+const Stationary: string[] = ["Pen", "Pencil", "Sharper","Scale"];
 
-function getToys() {
+function allThings() {
   return new Promise<string[]>((resolve) => {
-    resolve(toysInventory);
+    resolve(Stationary);
   });
 }
 
-function addToy(toyName: string) {
+function addThings(ThingName: string) {
   return new Promise<string[]>((resolve) => {
-    toysInventory.push(toyName);
-    resolve(toysInventory);
+    Stationary.push(ThingName);
+    resolve(Stationary);
+  });
+}
+
+function replaceThing(oldThingName: string, newName: string) {
+  return new Promise<string[]>((resolve, rejects) => {
+    for (let i = 0; i < Stationary.length; i++) {
+      if (Stationary[i] == oldThingName) {
+        Stationary[i] = newName;
+        resolve(Stationary);
+        i = Stationary.length;
+      }
+    }
+    rejects(Stationary);
+  });
+}
+function deleteThings(Delete1: number,Delete2: number) {
+  return new Promise<string[]>((resolve) => {
+    Stationary.splice(Delete1,Delete2);
+    resolve(Stationary);
   });
 }
 
@@ -22,35 +41,65 @@ function connectDB() {
   });
 }
 
-const toyDB: {
-  getToys: () => Promise<string[]>;
+const StationaryDB: {
+  deleteThings: (Delete1: number,Delete2: number) => Promise<string[]>;
+  replaceThing:(oldThingName: string, newName: string) => Promise<string[]>;
+  allThings: () => Promise<string[]>;
   connectDB: () => Promise<boolean>;
-  addToy: (toyName: string) => Promise<string[]>;
+  addThings: (ThingName: string) => Promise<string[]>;
 } = {
-  getToys,
+  allThings,
   connectDB,
-  addToy,
+  addThings,
+  deleteThings,
+  replaceThing,
 };
 
 export async function PUT(request: NextRequest) {
   try {
-    await toyDB.connectDB();
+    await StationaryDB.connectDB();
     const requestBody = await request.json();
-    const toys = await toyDB.addToy(requestBody.toyName);
-    return Response.json({ toys });
+    const Things = await StationaryDB.addThings(requestBody.ThingName);
+    return Response.json({ Things });
   } catch (error) {
     return Response.json({ error });
   }
 }
 
+export async function POST(request: NextRequest) {
+  try {
+    await StationaryDB.connectDB();
+    const requestBody = await request.json();
+    const Things = await StationaryDB.replaceThing(
+      requestBody.oldThingName,
+      requestBody.newName,
+    );
+    return Response.json({ Things });
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
 export async function GET() {
   try {
-    await toyDB.connectDB();
-    const toys = await toyDB.getToys();
-    return Response.json({ toys });
+    await StationaryDB.connectDB();
+    const Things = await StationaryDB.allThings();
+    return Response.json({ Things });
   } catch (error) {
     return Response.json({ error });
   }
 }
 
-// Create a shop to add, edit, delete and list the products
+export async function DELETE(request:NextRequest) {
+  try {
+    await StationaryDB.connectDB();
+    const requestBody = await request.json();
+    const Things = await StationaryDB.deleteThings(
+      requestBody.Delete1,
+      requestBody.Delete2,
+    );
+    return Response.json({ Things });
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
+
