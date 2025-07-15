3a2bd5bd2f5c65ad2535ebb235022b1a6da15247
shravansudo <shravansanthanam@gmail.com>
2025-07-15 10:33:15 -0600

    I learned Set time out!!!!!!!

--- src/app/exp/route.ts
+++ src/app/exp/route.ts
@@ -1,24 +1,29 @@
-import { NextRequest } from "next/server";
-function et1(request: NextRequest) {
-  const a = request.nextUrl.searchParams.get("a");
-  const b = request.nextUrl.searchParams.get("b");
-  const c = request.nextUrl.searchParams.get("c");
-  return Number(a) * (Number(b) * Number(c));
-}
-export function et(request: NextRequest) {
-  const A = et1(request);
-  return A;
-}
-// export function GET() {
-//   for (let i = 0; i < 10000; i++) {
-//     console.info(i);
-//   }
-//   console.log("time");
-//
+// import { NextRequest } from "next/server";
+// function et1(request: NextRequest) {
+//   const a = request.nextUrl.searchParams.get("a");
+//   const b = request.nextUrl.searchParams.get("b");
+//   const c = request.nextUrl.searchParams.get("c");
+//   return Number(a) * (Number(b) * Number(c));
 // }
+// export function et(request: NextRequest) {
+//   const A = et1(request);
+//   return A;
+// }
+// // export function GET() {
+// //   for (let i = 0; i < 10000; i++) {
+// //     console.info(i);
+// //   }
+// //   console.log("time");
+// //
+// // }
 
+// export function GET() {
+//   console.info("I love You !!");
+//   return Response.json("I love You !!");
+// }
+// setTimeout(GET, 1000);
 export function GET() {
-  console.info("I love You !!");
-  return Response.json("I love You !!");
+  console.info("hi");
+  return Response.json("hi");
 }
-setTimeout(GET, 1000);
+console.info(setTimeout(GET, 2500));
