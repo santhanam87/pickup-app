d15c4e3796ab939f43123e445d6e34239189215f
Santhanam Elumalai <sansandy04@gmail.com>
2025-07-26 16:42:04 -0600

    user crud

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,11 +1,43 @@
-import Page from "@app/model/page.model";
 import dbConnect from "@app/mongoose-connection";
+import { NextRequest } from "next/server";
+import User from "@app/model/user.model";
+
+// CRUD
+
+export async function PUT(request: NextRequest) {
+  try {
+    await dbConnect();
+    const userPayload = await request.json();
+    // const userInstance = new User(userPayload);
+    const user = await User.insertOne(userPayload);
+    return Response.json(user);
+  } catch (e) {
+    return Response.json(e, { status: 500 });
+  }
+}
+
+// url -> payload -> callback (fistinput)
+
+export async function POST(request: NextRequest) {
+  try {
+    const { _id, ...userPayload } = await request.json();
+    if (!_id) {
+      throw new Error("ID is missing");
+    }
+    await dbConnect();
+    const userUpdateResponse = await User.updateOne({ _id }, userPayload);
+    return Response.json(userUpdateResponse);
+  } catch (error) {
+    console.info(error);
+    return Response.json(error, { status: 500 });
+  }
+}
 
 export async function GET() {
   try {
     await dbConnect();
-    const page = await Page.find();
-    return Response.json(page);
+    const users = await User.find({ email: "shravan@in.com" });
+    return Response.json(users);
   } catch (e) {
     return Response.json(e, { status: 500 });
   }
