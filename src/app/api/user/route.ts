a398c358c96270dd159234a3deeaa63befbe2f90
shravansudo <shravansanthanam@gmail.com>
2025-07-28 14:28:06 -0600

    Practicing
    I deleted each function then redid them by my self (Exempt GET and DELETE because GET is very simple and DELETE is the same as update).

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,33 +3,28 @@ import { NextRequest } from "next/server";
 import User from "@app/model/user.model";
 
 // CRUD
-
 export async function PUT(request: NextRequest) {
   try {
-    await dbConnect();
     const userPayload = await request.json();
-    // const userInstance = new User(userPayload);
-    const user = await User.insertOne(userPayload);
+    const user = await User.insertone(userPayload);
     return Response.json(user);
-  } catch (e) {
-    return Response.json(e, { status: 500 });
+  } catch (error) {
+    return Response.json(error, { status: 500 });
   }
 }
-
 // url -> payload -> callback (fistinput)
 
 export async function POST(request: NextRequest) {
   try {
+    await dbConnect();
     const { _id, ...userPayload } = await request.json();
     if (!_id) {
-      throw new Error("ID is missing");
+      throw new Error("id is missing!");
     }
-    await dbConnect();
-    const userUpdateResponse = await User.updateOne({ _id }, userPayload);
-    return Response.json(userUpdateResponse);
-  } catch (error) {
-    console.info(error);
-    return Response.json(error, { status: 500 });
+    const updateUserInfo = await User.updateone({ _id }, userPayload);
+    return Response.json(updateUserInfo);
+  } catch (e) {
+    return Response.json(e, { status: 500 });
   }
 }
 
